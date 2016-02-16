c9a80f28751a40c500d5251baf5e96acfeacf184
alexandrius.martinus@gmx.de <alex>
2016-02-16 22:47:46 +0100

    user form and better styling

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(){
     return this.store.findAll("user");
+  },
+  actions:{
+    createUser(name,age){
+      var user=this.store.createRecord("user",{
+        name:name,
+        age:age
+      });
+      user.save();
+    }
   }
 });
