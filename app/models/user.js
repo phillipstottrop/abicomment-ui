c7286f5fa563822ea441c63dc80f040a1148074c
alexandrius.martinus@gmx.de <alex>
2016-06-09 18:15:24 +0200

    Add forename to user

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  forename:DS.attr('string'),
   name: DS.attr('string'),
   age: DS.attr('number'),
   comments: DS.hasMany('comment',{inverse:"user"}),
