c91587e5f84dcb862b1afb622da131f69b37eb67
alexandrius.martinus@gmx.de <alex>
2016-07-04 18:40:21 +0200

    added all teachers and all courses to poll-creator

--- app/components/poll-form.js
+++ app/components/poll-form.js
@@ -8,6 +8,24 @@ export default Ember.Component.extend({
 
     return (topic && this.get("poll.options").length>1 && !this.get("saving"));
   }.property("poll.topic","poll.options","saving"),
+  fuzzyCompare(term1,term2){
+    term1=term1.toLowerCase();
+    term2=term2.toLowerCase();
+
+    return (term1===term2);
+  },
+  fuzzySearch(term,arr){
+    var that=this;
+    var result=false;
+    arr.forEach(function(o){
+
+      if(that.fuzzyCompare(term,o)){
+        result=true;
+      }
+    });
+    return result;
+  },
+
   actions:{
     createOption(title){
       var poll=this.get("poll");
@@ -46,12 +64,54 @@ export default Ember.Component.extend({
 
     store.findAll("user").then(function(users){
       users.forEach(function(user){
-        var fullName=user.get("forename")+" "+user.get("name");
+
+        var fullName=user.get("fullname");
         var option=store.createRecord("option",{
           title:fullName,
         });
         poll.get("options").pushObject(option);
 
+      });
+    });
+  },
+  allCourses(){
+    var poll=this.get("poll");
+    var store=this.get("store");
+
+    store.findAll("course").then(function(courses){
+      courses.forEach(function(course){
+
+        var name=course.get("name")+" bei "+course.get("teacher");
+        var option=store.createRecord("option",{
+          title:name,
+        });
+        poll.get("options").pushObject(option);
+
+      });
+    });
+  },
+  allTeachers(){
+    var poll=this.get("poll");
+    var store=this.get("store");
+    var options=poll.get("options");
+
+    var names=options.map(function(o){
+      return o.get("title")
+    });
+
+    var arr=[];
+    var that=this;
+    store.findAll("course").then(function(courses){
+      courses.forEach(function(course){
+
+        var teacher=course.get("teacher");
+        //console.log(that.fuzzySearch(teacher,names));
+        if(!that.fuzzySearch(teacher,names)){
+        var option=store.createRecord("option",{
+          title:teacher,
+        });
+        poll.get("options").pushObject(option);
+      }
       });
     });
   },
