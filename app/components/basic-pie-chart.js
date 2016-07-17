af1ddb9bf62e6f02e619ddb256d86625b86dc3c2
alexandrius.martinus@gmx.de <alex>
2016-07-17 16:36:50 +0200

    fix for poll mouseovers not being displayed correctly

--- app/components/basic-pie-chart.js
+++ app/components/basic-pie-chart.js
@@ -71,9 +71,6 @@ this.update();
     .attr("fill", function(d, i) { return colors(i); })
     .attr("d", arc)
     .on("mouseover",function(d){
-      console.log(data);
-      console.log(d3.sum(data,function(o){return o.value;}));
-      console.log(Math.round(100*(d.data.value*100/d3.sum(data,function(o){return o.value;})))/100+"%)");
       tooltip.transition().duration(200)
       .style("opacity",0.9);
       tooltip.html(d.data.key+" : "+d.value+" Stimmen ("+ Math.round(100*(d.data.value*100/d3.sum(data,function(o){return o.value;})))/100+"%)")
