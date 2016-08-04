d8371e7a8c54cb116f2f82181b9e2f0eb39a907e
alexandrius.martinus@gmx.de <alex>
2016-08-04 15:14:13 +0200

    added mottos

---
+++ app/controllers/mottos.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  queryParams: ['limit'],
+  limit: 20,
+
+  sortBy:['upvotes:desc','id:asc'],
+  orderedMottos:Ember.computed.sort("model","sortBy"),
+  total: function(){
+    var total =this.get('model.meta').total || 20;
+    return total;
+  }.property('model.meta'),
+
+  notShowingAll:function(){
+    return this.get('total') > this.get('model.length');
+  }.property('total','model'),
+});
