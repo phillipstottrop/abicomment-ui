ee794d33fe96d1c1da826e64f78c584f11e1e4d4
alexmorten <alexandrius.martinus@gmx.de>
2016-08-31 00:04:30 +0200

    added search box for poll-options

--- app/components/single-poll.js
+++ app/components/single-poll.js
@@ -2,6 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  filter: '' ,
+  filteredOptions:function(){
+    var options = this.get("poll.options").filter((option)=>{
+
+      if(!option.get("title")  ){
+        return false;
+      }
+      if(this.get("filter").length<1){
+        return true;
+      }
+      var title=option.get("title").toLowerCase();
+      var filter=this.get("filter").toLowerCase();
+      return title.indexOf(filter)>-1;
+    });
+
+    return options;
+  }.property("poll.options.@each.title","filter"),
   actions:{
     vote(option){
       if(option){
