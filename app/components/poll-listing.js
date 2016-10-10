9c256987af7782fda87c9c3992e42962e48d1145
alexmorten <alexandrius.martinus@gmx.de>
2016-10-10 19:21:40 +0200

    fixed poll sorting

--- app/components/poll-listing.js
+++ app/components/poll-listing.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  sortBy:['id:desc'],
-  sortedPolls:Ember.computed.sort("polls","sortBy"),
-  
+  //sortBy:['id:desc'],
+  //sortedPolls:Ember.computed.sort("polls","sortBy"),
+  sortedPolls:function(){
+    var polls = this.get("polls");
+    return polls.map(function(p){return p}).sort(function(a,b){
+      return +b.get("id") - +a.get("id");
+    });
+    }.property("polls"),
   actions:{
     voteForOption(option){
       if(option){
