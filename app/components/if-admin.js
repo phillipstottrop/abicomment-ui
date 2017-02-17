c7a8a6ea3d6c4a9b3c9d24696dfc87c8b7b43a92
alexmorten <alexandrius.martinus@gmx.de>
2017-02-17 13:46:16 +0100

    fixed if-admin

--- app/components/if-admin.js
+++ app/components/if-admin.js
@@ -5,11 +5,14 @@ export default Ember.Component.extend({
   admin:function(){
     var status = this.get("status") || "admin";
     var id=this.get("user.id") || -1;
-    return (this.getResponseJSON().status === "admin" || this.getResponseJSON().status === status || this.getResponseJSON().id.toString()===id );
+    return (this.getResponseJSON().status === "admin" || this.isPartOf(this.getResponseJSON().status,status) || this.getResponseJSON().id.toString()===id );
   }.property("session","user"),
   getResponseJSON(){
 
     return this.get("session.data").authenticated.responseJSON;
   },
-
+  isPartOf(s1,s2){
+    
+    return s2.indexOf(s1) >= 0;
+  }
 });
